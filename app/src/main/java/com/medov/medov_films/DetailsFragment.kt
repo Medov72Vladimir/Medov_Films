be2dccec676ca46e9adeb812e04bdab7d3c74b08
Medov Vladimir <vlmedov@mail.ru>
2025-01-28 21:18:25 +0300

    module_25:не подгружается RV в Fragment

---
+++ app/src/main/java/com/medov/medov_films/DetailsFragment.kt
@@ -0,0 +1,32 @@
+package com.medov.medov_films
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.medov.medov_films.databinding.FragmentDetailsBinding
+
+class DetailsFragment : Fragment() {
+
+    private lateinit var binding: FragmentDetailsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setFilmsDetails()
+    }
+
+    private fun setFilmsDetails() {
+
+
+    }
+}
