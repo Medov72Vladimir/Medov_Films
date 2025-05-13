74fa415194b9917d037a228476d4db9bc7e5746b
Medov Vladimir <vlmedov@mail.ru>
2025-05-13 17:40:32 +0300

    37.7. Итоговый проект (-03)

---
+++ app/src/main/java/com/medov/medov_films/di/modules/DatabaseModule.kt
@@ -0,0 +1,15 @@
+package com.medov.medov_films.di.modules
+
+
+
+import com.medov.medov_films.data.MainRepository
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideRepository() = MainRepository()
+}
