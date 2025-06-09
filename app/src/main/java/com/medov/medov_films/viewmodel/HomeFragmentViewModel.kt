d77ba16ad9d4bf78f6b2d3ef047696e2349803d5
Medov Vladimir <vlmedov@mail.ru>
2025-06-09 20:58:43 +0300

    41.7. Итоговый проект (HW-03)

--- app/src/main/java/com/medov/medov_films/viewmodel/HomeFragmentViewModel.kt
+++ app/src/main/java/com/medov/medov_films/viewmodel/HomeFragmentViewModel.kt
@@ -6,23 +6,18 @@ import androidx.lifecycle.ViewModel
 import com.medov.medov_films.App
 import com.medov.medov_films.data.Entity.Film
 import com.medov.medov_films.domain.Interactor
-import java.util.concurrent.Executors
 import javax.inject.Inject
 
 class HomeFragmentViewModel : ViewModel() {
-
     val showProgressBar: MutableLiveData<Boolean> = MutableLiveData()
-
     @Inject
     lateinit var interactor: Interactor
     val filmsListLiveData: LiveData<List<Film>>
-
     init {
         App.instance.dagger.inject(this)
         filmsListLiveData = interactor.getFilmsFromDB()
         getFilms()
     }
-
     fun getFilms() {
         showProgressBar.postValue(true)
         interactor.getFilmsFromApi(1, object : ApiCallback {
@@ -35,7 +30,6 @@ class HomeFragmentViewModel : ViewModel() {
             }
         })
     }
-
     interface ApiCallback {
         fun onSuccess()
         fun onFailure()
